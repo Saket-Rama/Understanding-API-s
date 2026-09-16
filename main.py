c38d496c3d1a8d9129e097e97e04321b3bd4e96f
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-09-16 15:53:33 +0530

    added another endpoint

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Saket"}
 
+@app.get("/Name")
+def read_root():
+    return{"Hello":"Rama"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
